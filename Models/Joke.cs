28f86faff9e7c1bf2f0c88e4ff1c7b959068201b
Nicholas Muchelemba <51362195+nicholas124@users.noreply.github.com>
2021-03-26 23:31:19 +0200

    Add files via upload

---
+++ Models/Joke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesSite.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+                   
+        }
+    }
+}
